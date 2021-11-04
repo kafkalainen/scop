3d371e47e6aa33fae2661970142947993b4bd69f
kafkalainen <joonas.hj.nivala@gmail.com>
2021-11-04 15:08:01 +0200

    Cleaned StandardShading.fs from comments, added spec property.

--- assets/shaders/StandardShading.fs
+++ assets/shaders/StandardShading.fs
@@ -17,41 +17,19 @@ uniform float LightPower;
 
 void main()
 {
-	// Material properties
 	vec3 MaterialDiffuseColor = mix(texture(texture1, UV), texture(texture2, UV), 0.2f).rgb;
 	vec3 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
 	vec3 MaterialSpecularColor = vec3(0.3,0.3,0.3);
-
-	// Distance to the light
-	float distance = length( LightPosition_worldspace - Position_worldspace );
-
-	// Normal of the computed fragment, in camera space
-	vec3 n = normalize( Normal_cameraspace );
-	// Direction of the light (from the fragment to the light)
-	vec3 l = normalize( LightDirection_cameraspace );
-	// Cosine of the angle between the normal and the light direction,
-	// clamped above 0
-	//  - light is at the vertical of the triangle -> 1
-	//  - light is perpendicular to the triangle -> 0
-	//  - light is behind the triangle -> 0
-	float cosTheta = clamp( dot( n,l ), 0,1 );
-
-	// Eye vector (towards the camera)
-	vec3 E = normalize(EyeDirection_cameraspace);
-	// Direction in which the triangle reflects the light
-	vec3 R = reflect(-l,n);
-	// Cosine of the angle between the Eye vector and the Reflect vector,
-	// clamped to 0
-	//  - Looking into the reflection -> 1
-	//  - Looking elsewhere -> < 1
-	float cosAlpha = clamp( dot( E,R ), 0,1 );
-
+	float distance = length(LightPosition_worldspace - Position_worldspace);
+	vec3 normal = normalize(Normal_cameraspace);
+	vec3 lightDir = normalize(LightDirection_cameraspace);
+	float cosTheta = clamp(dot(normal, lightDir), 0, 1);
+	vec3 view_dir = normalize(EyeDirection_cameraspace);
+	vec3 reflect_dir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 32);
 	color.a = 0.3;
 	color.rgb =
-		// Ambient : simulates indirect lighting
 		MaterialAmbientColor +
-		// Diffuse : "color" of the object
-		MaterialDiffuseColor * LightColor * LightPower * cosTheta / (distance*distance) +
-		// Specular : reflective highlight, like a mirror
-		MaterialSpecularColor * LightColor * LightPower * pow(cosAlpha,5) / (distance*distance);
+		MaterialDiffuseColor * LightColor * LightPower * cosTheta / (distance * distance) +
+		MaterialSpecularColor * LightColor * LightPower * spec / (distance * distance);
 }
